cbfb61c1691fd4b92ee46b8e9775663a4935110b
Oran9eUtan <oran9eutan@gmail.com>
2022-01-26 01:13:51 +0100

    fix: Smaller category title fonts size

--- src/lib/components/HatList.svelte
+++ src/lib/components/HatList.svelte
@@ -11,7 +11,7 @@
 <div class="max-w-4xl w-full">
 	{#if !isEmptyObject($language) && !isEmptyObject(groups)}
 		{#each Object.entries(groups) as [groupName, hats]}
-			<h1 class="text-2xl text-left py-4">
+			<h1 class="text-xl text-left py-4">
 				{groupName.trim().replace(/^\w/, (c) => c.toUpperCase())}
 			</h1>
 			<div class="grid gap-4 md:grid-cols-5 sm:grid-cols-4 grid-cols-3 items-start">
@@ -22,7 +22,7 @@
 		{/each}
 	{:else}
 		{#each [[10, 8], [20, 5], [15, 5], [30, 10]] as group}
-			<Skeleton class="my-4 h-8" style={`width: ${group[0]}rem`} />
+			<Skeleton class="my-4 h-7" style={`width: ${group[0]}rem`} />
 			<div class="grid gap-4 md:grid-cols-5 sm:grid-cols-4 grid-cols-3 items-start">
 				{#each Array(group[1]) as i}
 					<Skeleton class="w-30 h-28" />
