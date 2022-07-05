3b7bacbaaff2aea25078cef6e4ee6a8fec12e7e3
OrangeUtan <michaelbrunn3r@gmail.com>
2022-07-05 14:00:31 +0200

    build: Setup netlify adapter

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 - [Sveltekit on Netlify](https://docs.netlify.com/integrations/frameworks/sveltekit/)
 - [Use pnpm with Netlify](https://www.seancdavis.com/posts/use-pnpm-with-netlify/)
+- [adapter-netlify](https://github.com/sveltejs/kit/tree/master/packages/adapter-netlify)
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      // Create a Netlify Edge Function rather than using standard Node-based functions
+      edge: false,
+
+      // Split your app into multiple functions instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
+    })
+  }
 };
 
 export default config;
