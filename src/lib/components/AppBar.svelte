905b5451cf4b5876fb187ec040704fbe1db0eb88
OrangeUtan <michaelbrunn3r@gmail.com>
2022-07-07 17:16:46 +0200

    feat: Changed menu icon

--- src/lib/components/AppBar.svelte
+++ src/lib/components/AppBar.svelte
@@ -19,16 +19,17 @@
     <div class="dropdown">
       <i tabindex="0" class="btn btn-ghost lg:hidden">
         <svg
+          width="20"
+          height="20"
           xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="currentColor"
+          class="inline-block h-5 w-5 stroke-current md:h-6 md:w-6"
           ><path
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M4 6h16M4 12h8m-8 6h16"
+            d="M4 6h16M4 12h16M4 18h16"
           /></svg
         >
       </i>
