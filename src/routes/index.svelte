7d82deb389f5018ba943c90adcde14b01b7e5897
OrangeUtan <michaelbrunn3r@gmail.com>
2022-07-08 15:02:56 +0200

    feat: Added wavy hero background

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,14 +2,13 @@
   import Logo from '$components/Icons/Logo.svelte';
   import Menu from 'svelte-material-icons/Menu.svelte';
   import Github from 'svelte-material-icons/Github.svelte';
-  import ThemeIcon from 'svelte-material-icons/ThemeLightDark.svelte';
   import ThemeToggle from '$components/ThemeToggle.svelte';
 </script>
 
 <div class="drawer">
   <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content flex flex-col">
-    <div class="navbar">
+  <div class="drawer-content">
+    <div class="navbar fixed text-primary-content">
       <div class="navbar-start">
         <!-- Logo desktop -->
         <a class="btn btn-ghost normal-case text-xl hidden md:flex" href="/">
@@ -49,13 +48,22 @@
         </div>
       </div>
     </div>
-
-    <!-- Page content here -->
-    Content
+    <div
+      class="w-full pt-16 bg-gradient-to-br from-primary to-secondary text-primary-content grid place-items-center items-end"
+    >
+      <div class="hero-content flex-col">
+        <h1 class="text-4xl font-extrabold">Hats</h1>
+      </div>
+      <svg class="fill-secondary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          d="M0,192L48,197.3C96,203,192,213,288,224C384,235,480,245,576,234.7C672,224,768,192,864,186.7C960,181,1056,203,1152,192C1248,181,1344,139,1392,117.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+        />
+      </svg>
+    </div>
   </div>
   <div class="drawer-side">
     <label for="my-drawer-3" class="drawer-overlay" />
-    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
+    <ul class="menu p-2 overflow-y-auto w-80 bg-base-100">
       <!-- Sidebar content here -->
       <li><a href="index">Index</a></li>
     </ul>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,21 @@ const config = {
   theme: {
     extend: {}
   },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
+          primary: '#edb12b',
+          secondary: '#f64669',
+          'primary-content': '#f8f8f8'
+        },
+        dark: {
+          ...require('daisyui/src/colors/themes')['[data-theme=dark]']
+        }
+      }
+    ]
+  },
 
   plugins: [require('daisyui')]
 };
