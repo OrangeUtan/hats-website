d194825547d858f48fa0fda928060f0991244b2e
OrangeUtan <michaelbrunn3r@gmail.com>
2022-07-08 15:09:15 +0200

    feat: Click to cycle through splash texts

--- src/lib/stores/splash.ts
+++ src/lib/stores/splash.ts
@@ -3,8 +3,13 @@ import { writable } from 'svelte/store';
 const splash_texts = [
   "Gotta Get 'Em All",
   'Take your pet for a walk',
-  'Pantastic headwear',
+  'Pantastic headgear',
   '5/7 Opticians approve'
 ].sort(() => 0.5 - Math.random());
+let current_splash_idx = 0;
 
-export const splash_text = writable(splash_texts[0]);
+export const splash_text = writable(splash_texts[current_splash_idx]);
+
+export function next_splash() {
+  splash_text.set(splash_texts[current_splash_idx++ % splash_texts.length]);
+}
