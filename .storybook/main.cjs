30421c62a6c338a39ffb736f4a9d23a365a2c60f
OrangeUtan <michaelbrunn3r@gmail.com>
2022-07-10 16:43:54 +0200

    build: Removed storybook/addon-postcss

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -5,21 +5,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        cssLoaderOptions: {
-          // When you have splitted your css over multiple files
-          // and use @import('./other-styles.css')
-          importLoaders: 1
-        },
-        postcssLoaderOptions: {
-          // When using postCSS 8
-          implementation: require('postcss')
-        }
-      }
-    }
+    '@storybook/addon-interactions'
   ],
   framework: '@storybook/svelte',
   core: {
