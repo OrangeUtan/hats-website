dbf7367db287fac61579730d67ffe7b05baca01a
OrangeUtan <michaelbrunn3r@gmail.com>
2022-07-12 13:05:11 +0200

    feat: Added basic error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" context="module">
+  import type { Load } from '@sveltejs/kit';
+
+  /** @type {import('@sveltejs/kit').Load} */
+  export const load: Load = ({ error, status }) => {
+    return {
+      props: {
+        message: `${status}: ${error?.message}`
+      }
+    };
+  };
+</script>
+
+<script lang="ts">
+  export let message: string;
+</script>
+
+<div class="w-full h-full flex justify-center items-center bg-primary">
+  <h1 class="text-secondary-content text-4xl">{message}</h1>
+</div>
