6759ee6177be3d151c522a2c6a9e5ebc1322a551
OrangeUtan <michaelbrunn3r@gmail.com>
2022-07-12 18:42:59 +0200

    build: Update Typewriter

--- src/lib/components/Splash/Splash.svelte
+++ src/lib/components/Splash/Splash.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import { splash_text, next_splash } from '$data/splash';
-  import Typewriter from 'svelte-typewriter/src/Typewriter.svelte';
+  import Typewriter from 'svelte-typewriter';
 
   export let cls = '';
 </script>
 
 <!-- Ensures component doesn't change size in the first frame  -->
-<div>
+<div class={cls}>
   <Typewriter cursor={false} interval={40}>
-    <p class={`cursor-pointer ${cls}`} on:click={next_splash}>
+    <p class="cursor-pointer" on:click={next_splash}>
       {$splash_text}
     </p>
   </Typewriter>
@@ -16,6 +16,6 @@
 
 <style>
   div {
-    min-height: 3rem;
+    min-height: 2.3rem;
   }
 </style>
