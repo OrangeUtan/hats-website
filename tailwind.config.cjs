055710c3102d6695fd7fff32ca3290b58d9f5244
OrangeUtan <michaelbrunn3r@gmail.com>
2022-07-14 22:53:25 +0200

    feat: Auto dark mode
    Will automatically switch between light-/dark-mode if data-theme is an unknown theme (e.g. "auto").

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,35 +1,17 @@
-const config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.svelte'],
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  darkMode: ['[data-mode="dark"]'],
   theme: {
-    extend: {
-      colors: {
-        primary: 'var(--primary)',
-        'primary-content': 'var(--primary-content)',
-        secondary: 'var(--secondary)',
-        'secondary-content': 'var(--secondary-content)',
-        accent: 'var(--accent)',
-        'accent-content': 'var(--accent-content)',
-        neutral: 'var(--neutral)',
-        'neutral-content': 'var(--neutral-content)',
-        'base-100': 'var(--base-100)',
-        'base-200': 'var(--base-200)',
-        'base-300': 'var(--base-300)',
-        info: 'var(--info)',
-        success: 'var(--success)',
-        warning: 'var(--warning)',
-        error: 'var(--error)'
-      }
-    },
     fontFamily: {
       title: ['Minecrafter', 'sans-serif'],
       'title-alt': ['Minecrafter Alt', 'sans-serif'],
       mono: ['ui-monospace', 'SFMono-Regular', 'Monaco', 'Consolas', 'Courier New', 'monospace']
     }
   },
+  daisyui: {
+    themes: []
+  },
   plugins: [require('daisyui')]
 };
-
-module.exports = config;
